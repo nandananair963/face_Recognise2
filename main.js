aeba52fc616be3565de8b567969a76efc47c6e44
nandananair963 <80956630+nandananair963@users.noreply.github.com>
2023-05-16 17:01:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    image_format:"png",
+    png_quality:90
+ });
+ 
+ camera=document.getElementById("camera");
+ Webcam.attach(camera);
+ 
+ function take_snapshot(){
+     Webcam.snap(function(data_uri){
+         document.getElementById("result").innerHTML='<img id="captured_img" src="'+ data_uri +'"/>';
+ 
+     });
+ }
+ 
+ console.log('ml5 version',ml5.version);
+ 
+ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json',modelLoaded);
